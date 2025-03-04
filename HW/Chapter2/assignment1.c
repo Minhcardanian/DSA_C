423f638f4d79dd25aa6f292600c12ee9fa026cbd
Minhcadarnian <markbuicadarnian@gmail.com>
2025-03-04 09:52:41 +0700

    c1/as1 added

---
+++ HW/Chapter2/assignment1.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+//swap function using pointers
+void Swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+//main 
+int main(){
+    int a =10, b=20;
+    printf("Before swapping: a=%d, b=%d\n", a,b);
+    Swap(&a, &b);
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0; 
+}
